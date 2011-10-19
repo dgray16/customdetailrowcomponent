e6aaf46bf9debe9b5871db187a6515130862a0ee
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2011-10-19 18:47:18 +0200

    Modified version number to 1.0.0.

--- src/main/java/com/igalia/java/zk/components/customdetailrowcomponent/Version.java
+++ src/main/java/com/igalia/java/zk/components/customdetailrowcomponent/Version.java
@@ -3,5 +3,5 @@ package com.igalia.java.zk.components.customdetailrowcomponent;
 public class Version {
 	/** Returns the version UID.
 	 */
-	public static final String UID = "0.0.1-SNAPSHOT";
+	public static final String UID = "1.0.0";
 }
